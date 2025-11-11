a87d3db947f3e7edba1bb4d9920658004936f84f
hao <alveinwang@163.com>
2025-11-11 17:24:36 +0800

    modified:   .github/workflows/deploy.yml 	modified:   app/layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ export const metadata: Metadata = {
   title: 'H!NT Lab | 实验室主页',
   description: '人工智能、人机协同认知、自然语言处理实验室主页',
   icons: [{ rel: 'icon', url: 'favicon.svg' }],
-  metadataBase: new URL('https://hint-lab.github.io/people/wang_hao')
+  metadataBase: new URL('https://hint-lab.github.io')
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
